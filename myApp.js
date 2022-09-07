c3f57806ee6b598d098f98740edcd39b5ccc4da4
Taiwo <ayeola05@gmail.com>
2022-09-07 14:50:48 +0100

    third exc

--- myApp.js
+++ myApp.js
@@ -1,10 +1,15 @@
 let express = require("express");
 let app = express();
+let path = require("path");
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+absolutePath = path.join(__dirname, "views/index.html");
 
 console.log("Hello World");
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(absolutePath);
 });
 
 module.exports = app;
